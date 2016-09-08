513b16c39ce4685abed55257cb305aef85486143
7Kart <artgor92@gmail.com>
2016-09-08 12:52:39 +0300

    update galary

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,21 @@
 jQuery(document).ready(function(){
-    console.log("script compin and run");
     $('#fullpage').fullpage({
         sectionsColor:[null,"#1BBF7C","#7E8F7C","#1BBF7C"],
         anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage'],
         slidesNavigation:true,
-        menu: '#mainMenu'
+        menu: '#mainMenu',
+        scrollOverflow:true,
+        afterLoad: function(anchorLink, index){
+            if(index == 2){
+                $('.massonry-grid').masonry({
+                    itemSelector: '.item'
+                });
+            }
+        }
     });
 
     $('a[data-rel^=lightcase]').lightcase({
-        maxHeight:1000,
+        maxHeight:1600,
         labels:{
           'errorMessage': 'Ресурс не найден...',
           'sequenceInfo.of': ' из ',
@@ -17,24 +24,11 @@ jQuery(document).ready(function(){
           'navigator.next': 'Пред',
           'navigator.play': 'Старт',
           'navigator.pause': 'Пауза'
-        },
-        //
-        // showSequenceInfo: false,
-        // iframe: {
-        //     width: 'auto',
-        //     height: 'auto',
-        //     frameborder: 0
-        // }
+        }
     });
 
 
 
-    $('.massonry-grid').masonry({
-        // set itemSelector so .grid-sizer is not used in layout
-        itemSelector: '.grid-item',
-        // use element for option
-        columnWidth: '.grid-sizer',
-        percentPosition: true
-    })
+
 
 });
